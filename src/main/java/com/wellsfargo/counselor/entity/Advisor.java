4b455931871233b8935e16aba351921ca1ada191
Rod1on777 <rodionk533@gmail.com>
2024-01-11 17:37:05 +1100

    Submission

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -84,3 +84,227 @@ public class Advisor {
         this.email = email;
     }
 }
+
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue()
+    private long clientId;
+    
+    @Column(nullable = false)
+    private String advisorId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String email;
+
+    protected Client() {
+
+    }
+
+    public Client(String advisorId, String firstName, String lastName, String address, String phone, String email) {
+    	this.advisorId = advisorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public String getAdvisorId() {
+        return advisorId;
+    }
+
+    public void setAdvisorId(String advisorId) {
+        this.advisorId = advisorId;
+    }
+    
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+
+
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String clientId;
+
+    @Column(nullable = false)
+    private String creationDate;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(String clientId, String creationDate) {
+        this.clientId = clientId;
+        this.creationDate = creationDate;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+}
+
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private String portfolioId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String purchasePrice;
+
+    @Column(nullable = false)
+    private String purchaseDate;
+
+    @Column(nullable = false)
+    private String quantity;
+    
+    protected Security() {
+
+    }
+
+    public Security(String portfolioId, String name, String category, String purchasePrice, String purchaseDate, String quantity) {
+        this.portfolioId = portfolioId;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    public String getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(String portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(String purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+    
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
+
